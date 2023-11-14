c412ae51b38e8dd95ac733c3733a7281f0d0f144
Rafael Vendramini <72450490+rafaelbvc@users.noreply.github.com>
2023-11-14 05:12:03 -0300

    v. 0.0.1 - First setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
-    <main className="App">
-    </main>
+    <main className="bg-black w-screen h-screen">
+      <MainPage />
+    </main >
   );
 }
 
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainPage = () => {
+    return (
+        <div className='text-white'>My History - Cabala</div>
+    )
+}
+
+export default MainPage
